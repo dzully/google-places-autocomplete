6bcf558f690817e2f4765fe1f27dd7cb8534a58b
Dzul Syakimin <dzulsyakimin61@gmail.com>
2023-01-19 13:30:05 +0800

    chore:Update prettier json

--- lint-staged.js
+++ lint-staged.js
@@ -2,7 +2,7 @@ module.exports = {
   '*.{js,jsx,ts,tsx}': [
     'eslint --max-warnings=0',
     'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
-    () => 'tsc-files --noEmit',
+    () => 'tsc-files --noEmit'
   ],
-  '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
+  '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write']
 }
